e093d1fcf9f9a66d3a786c84cfade4c3649d94d5
sergey <sergey.natalenko@mail.ru>
2020-06-12 22:57:33 +0300

    Отступы, пробелы, комментарии

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 from src import mf_dcca_1
-a=[1,2,3,4,5]
-print(mf_dcca_1.f(a,3))
+
+a = [1, 2, 3, 4, 5]
+print(mf_dcca_1.f(a, 3))
--- src/mf_dcca_1.py
+++ src/mf_dcca_1.py
@@ -1,8 +1,11 @@
 import numpy as np
+
+
 def f(x, i):
+    # комментарии надо оставлять
     mean = np.mean(x)
-    a=0
-    for k in range (i):
+    a = 0
+    for k in range(i):
         a += x[k] - mean
     return a
 
