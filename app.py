7553558d62740710e1aa86e11aa04aec132627fb
Даша <dashafyd@ya.ru>
2020-06-12 22:34:19 +0300

    &

---
+++ app.py
@@ -0,0 +1,3 @@
+from src import mf_dcca_1
+a=[1,2,3,4,5]
+print(mf_dcca_1.f(a,3))
--- src/mf_dcca_1.py
+++ src/mf_dcca_1.py
@@ -1,4 +1,10 @@
-print("Hello, world!")
+import numpy as np
+def f(x, i):
+    mean = np.mean(x)
+    a=0
+    for k in range (i):
+        a += x[k] - mean
+    return a
 
 
 
